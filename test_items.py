b15ac97361fdba08fb816d9161569aeed2ff1b92
TRX <a@ext.com>
2020-04-06 17:39:22 +0300

    test item has add to cart button

--- test_items.py
+++ test_items.py
@@ -1,9 +1,9 @@
 import time
 
 
-def test_button_of_basket(browser, language):
-    link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
+def test_item_has_add_to_cart_button(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     #time.sleep(30)
-    button = browser.find_element_by_css_selector('button.btn-add-to-basket')
-    assert button.text is not None, 'Button of basket not found'
+    button = browser.find_elements_by_css_selector('button.btn-add-to-basket')
+    assert len(button) > 0, 'Button of basket not found'
